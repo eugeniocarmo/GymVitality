b48d8b71fc42cdbf1c79da5b42a070c338960e05
Eugenio Carmo <eugeniocarmo@gmail.com>
2023-05-23 18:29:24 +0100

    feat:"Adding fonts"

--- App.tsx
+++ App.tsx
@@ -1,22 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-
+import { Text, View } from 'react-native'
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 export default function App() {
+ const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{alignItems: 'center', justifyContent: 'center', flex:1}}>
+      {fontsLoaded ? <Text style={{fontFamily: 'Roboto_700Bold'}}>Hello World</Text> : <View />}
+      
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
