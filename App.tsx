fa708c97d00a1681c285da496461d43a9c9c1c89
Eugenio Carmo <eugeniocarmo@gmail.com>
2023-05-24 16:42:44 +0100

    feat: ' NativeBase installation'

--- App.tsx
+++ App.tsx
@@ -1,24 +1,20 @@
-import { Text, View, StatusBar } from 'react-native'
+import { View, StatusBar } from 'react-native'
+import { NativeBaseProvider } from 'native-base'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 export default function App() {
  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
   
  return (
-    <View style={{
-            alignItems: 'center', 
-            justifyContent: 'center', 
-            flex:1,
-            backgroundColor: '#202024'
-          }}>
+    <NativeBaseProvider>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="202024"
+        translucent
+      />
 
-    {fontsLoaded ? <Text style={{fontFamily: 'Roboto_700Bold'}}>Hello World</Text> : <View />}
-
-    <StatusBar 
-      barStyle="light-content"
-      backgroundColor="202024"
-      translucent
-    />
-    </View>
+      {fontsLoaded ? <View /> : <View />}
+ 
+    </NativeBaseProvider>
   );
 }
