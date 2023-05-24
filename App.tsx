8b7d0ada45de44b1c580cbaed0db9954501c2dc4
Eugenio Carmo <eugeniocarmo@gmail.com>
2023-05-24 09:13:13 +0100

    feat: ' Customising the StatusBar'

--- App.tsx
+++ App.tsx
@@ -1,14 +1,24 @@
-import { Text, View } from 'react-native'
+import { Text, View, StatusBar } from 'react-native'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 export default function App() {
  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
-  return (
-    <View style={{alignItems: 'center', justifyContent: 'center', flex:1}}>
-      {fontsLoaded ? <Text style={{fontFamily: 'Roboto_700Bold'}}>Hello World</Text> : <View />}
-      
+  
+ return (
+    <View style={{
+            alignItems: 'center', 
+            justifyContent: 'center', 
+            flex:1,
+            backgroundColor: '#202024'
+          }}>
+
+    {fontsLoaded ? <Text style={{fontFamily: 'Roboto_700Bold'}}>Hello World</Text> : <View />}
+
+    <StatusBar 
+      barStyle="light-content"
+      backgroundColor="202024"
+      translucent
+    />
     </View>
   );
 }
-
-
