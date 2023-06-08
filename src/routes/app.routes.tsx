bbe1684f4f3d944b5ccef5114c5dcd39c48da78f
Eugenio Carmo <eugeniocarmo@gmail.com>
2023-06-08 15:56:38 +0100

    feat: Creating the private app routes

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,33 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import { Home } from '@screens/Home';
+import { Profile } from '@screens/Profile';
+import { History } from '@screens/History';
+import { Exercise } from '@screens/Exercise';
+
+
+const { Navigator, Screen } = createBottomTabNavigator();
+
+export function AppRoutes() {
+
+  return (
+    <Navigator>
+      <Screen 
+        name="home"
+        component={Home}
+      />
+      <Screen 
+        name="history"
+        component={History}
+      />
+      <Screen 
+        name="profile"
+        component={Profile}
+      />
+      <Screen 
+        name="exercise"
+        component={Exercise}
+      />
+    </Navigator>
+  );
+}
---
+++ src/screens/Profile.tsx
@@ -0,0 +1,13 @@
+import { Text, Center } from 'native-base';
+
+export function Profile() {
+
+  return(
+    <Center flex={1}>
+      <Text>
+        Profile
+      </Text>
+    </Center>
+  );
+}
+
