204362078863be0d3ba17583593e27a20f006a22
Eugenio Carmo <eugeniocarmo@gmail.com>
2023-06-09 12:49:25 +0100

    feat: "Typing the App routes"

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,12 +1,23 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator, BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
 
 import { Home } from '@screens/Home';
 import { Profile } from '@screens/Profile';
 import { History } from '@screens/History';
 import { Exercise } from '@screens/Exercise';
 
+type AppRoutes = {
+  home: undefined;
+  exercise: undefined;
+  profile: undefined;
+  history: undefined;
+}
 
-const { Navigator, Screen } = createBottomTabNavigator();
+
+
+export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>;
+
+const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
+
 
 export function AppRoutes() {
 
