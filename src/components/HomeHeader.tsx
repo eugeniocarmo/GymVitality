fbf6d35b51180713f0dc50198c70bfbbc4d664d9
Eugenio Carmo <eugeniocarmo@gmail.com>
2023-06-13 18:36:05 +0100

    feat: "UserPhoto component"

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,9 +1,17 @@
-import { HStack, VStack, Text, Heading, Center } from "native-base";
+import { HStack, VStack, Text, Heading } from "native-base";
+
+import { UserPhoto } from "./UserPhoto";
 
 
 export function HomeHeader(){
   return(
     <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems={"center"}>
+      <UserPhoto 
+        source={{uri: "http://github.com/eugeniocarmo.png"}}
+        size={16}
+        alt="User photo"
+        mr={4}
+      />
       <VStack>
         <Text color="gray.100" fontSize="md">
           Hello,
