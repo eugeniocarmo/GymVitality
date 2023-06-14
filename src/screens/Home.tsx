d0c99c7477a09a5ad836ad45401ab9183d7fd161
Eugenio Carmo <eugeniocarmo@gmail.com>
2023-06-14 11:53:49 +0100

    feat: "Creating Group component"

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,14 @@
+import { HStack, VStack } from 'native-base';
+
 import { HomeHeader } from '@components/HomeHeader';
-import { VStack } from 'native-base';
+import { Group } from '@components/Group';
 
 export function Home() {
 
   return(
     <VStack flex={1}>
       <HomeHeader />
+      <Group name="back"/>
     </VStack>
   );
 }
