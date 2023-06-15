7349476f2eaf745a6e6d370c2547606cbd8b6d48
Eugenio Carmo <eugeniocarmo@gmail.com>
2023-06-15 18:59:41 +0100

    feat: 'Creating ScreenHeader Component'

---
+++ src/components/ScreenHeader.tsx
@@ -0,0 +1,16 @@
+import {  Heading, Center } from 'native-base';
+
+
+type Props = {
+  title: string;
+}
+
+export function ScreenHeader({title}: Props){
+  return(
+    <Center bg="gray.600" pt={16} pb={6} px={8} mb={5}>
+          <Heading color="gray.100" fontSize={'xl'} fontFamily={'heading'}> 
+            {title}
+          </Heading>
+    </Center>
+  );
+}
--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,13 +1,15 @@
-import { Text, Center } from 'native-base';
+import { Text, Center, VStack } from 'native-base';
+import { ScreenHeader } from '@components/ScreenHeader';
+
 
 export function History() {
-
-  return(
-    <Center flex={1}>
-      <Text color={'white'}>
-        History
-      </Text>
-    </Center>
+  return (
+    <VStack flex={1}>
+      <ScreenHeader title='Exercise History'/>
+      <Center>
+        <Text color={'white'}> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit sint suscipit eligendi perspiciatis quasi voluptatum assumenda tempora beatae non, explicabo, tenetur perferendis. Amet voluptate commodi perferendis quidem exercitationem nam magnam.</Text>
+      </Center>
+    </VStack>
   );
 }
 
