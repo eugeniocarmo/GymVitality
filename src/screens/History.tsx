b3013ab0c4818a4a84e4353fcdfa942e1c732383
Eugenio Carmo <eugeniocarmo@gmail.com>
2023-06-15 20:04:47 +0100

    feat: 'Component HistoryCard'

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,14 +1,17 @@
 import { Text, Center, VStack } from 'native-base';
+
 import { ScreenHeader } from '@components/ScreenHeader';
+import { HistoryCard } from '@components/HistoryCard';
 
 
 export function History() {
   return (
     <VStack flex={1}>
       <ScreenHeader title='Exercise History'/>
-      <Center>
-        <Text color={'white'}> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit sint suscipit eligendi perspiciatis quasi voluptatum assumenda tempora beatae non, explicabo, tenetur perferendis. Amet voluptate commodi perferendis quidem exercitationem nam magnam.</Text>
-      </Center>
+
+      <HistoryCard/>   
+      <HistoryCard/>
+      <HistoryCard/>
     </VStack>
   );
 }
