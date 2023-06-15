636d1aa5efe8e467b1db2dfe9d5e43b089fc6433
Eugenio Carmo <eugeniocarmo@gmail.com>
2023-06-15 09:25:16 +0100

    feat: 'Exercise list header'

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
-import { FlatList, VStack } from 'native-base';
+import { FlatList, HStack, Heading, Text, VStack } from 'native-base';
 
 import { HomeHeader } from '@components/HomeHeader';
 import { Group } from '@components/Group';
+
 
 export function Home() {
 
@@ -33,6 +34,18 @@ export function Home() {
         maxH={10}
        
       /> 
+      <VStack flex={1} px="8">
+        <HStack justifyContent="space-between" mb="5">
+          <Heading color="gray.200" fontSize="md">
+            Exercise
+          </Heading>
+          <Text color="gray.200" fontSize="sm">
+            4
+          </Text>
+        </HStack>
+      </VStack>    
+      
+
     </VStack>
   );
 } 
