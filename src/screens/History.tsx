1fb36e98690a3b8bab56ed86ac1ae4ba36216bed
Eugenio Carmo <eugeniocarmo@gmail.com>
2023-06-16 11:19:47 +0100

    feat: Adding SectionList

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,18 +1,50 @@
-import { Text, Center, VStack } from 'native-base';
+import { useState } from 'react';
+
+import { Heading, VStack, SectionList } from 'native-base';
 
 import { ScreenHeader } from '@components/ScreenHeader';
 import { HistoryCard } from '@components/HistoryCard';
 
 
+
 export function History() {
+
+  const [exercises, setExercises] = useState([
+
+    {
+      title: '16.06.23',
+      data: ['Front Pull', 'Unilateral rowing', 'Front pull', 'Bent-over row', ],
+    },
+    {
+      title: '17.06.23',
+      data: ['Front pull', 'Bent-over row',],
+    },
+    {
+      title: '18.06.23',
+      data: ['Deadlift'],
+    }
+
+  ]);
+
   return (
     <VStack flex={1}>
       <ScreenHeader title='Exercise History'/>
 
-      <HistoryCard/>   
-      <HistoryCard/>
-      <HistoryCard/>
+
+      <SectionList
+              sections={exercises}
+              keyExtractor={item => item}
+              renderItem={({ item }) => (
+                <HistoryCard/>   
+              )}
+              renderSectionHeader={({ section }) => (
+                <Heading color='gray.200' fontSize="md" mt="10" mb="3">
+                  {section.title}
+                </Heading>
+              )}
+              px={8}
+      />
+              
     </VStack>
   );
 }
-
