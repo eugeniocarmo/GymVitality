0fefa070f8ffc4f91237ecb45c61e54264235450
Eugenio Carmo <eugeniocarmo@gmail.com>
2023-06-16 22:24:46 +0100

    feat: 'Adding user photo on profile screen.'

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,11 +1,21 @@
 import { ScreenHeader } from '@components/ScreenHeader';
-import { VStack } from 'native-base';
+import { UserPhoto } from '@components/UserPhoto';
+import { Center, ScrollView, VStack } from 'native-base';
 
 export function Profile() {
 
   return(
     <VStack flex={1}>
       <ScreenHeader title="Profile"/>
+      <ScrollView>
+      <Center mt={2} px={10}>
+        <UserPhoto 
+          source={{uri: "http://github.com/eugeniocarmo.png"}}
+          size={33}
+          alt="User photo"
+        />
+      </Center>
+      </ScrollView>
     </VStack>
   );
 }
