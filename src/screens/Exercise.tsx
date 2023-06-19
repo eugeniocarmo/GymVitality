9de61e1742e118e0f06c902af683998e8ddc5e30
Eugenio Carmo <eugeniocarmo@gmail.com>
2023-06-19 10:46:41 +0100

    feat: Back button & header for the exercise screen

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,13 +1,30 @@
-import { Text, Center } from 'native-base';
+import { TouchableOpacity } from 'react-native';
+import { Icon, VStack } from 'native-base';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+
+import { AppNavigatorRoutesProps } from '@routes/app.routes';
+
 
 export function Exercise() {
 
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+  
+  function handleGoBack(){
+    navigation.goBack();
+  } 
+
   return(
-    <Center flex={1}>
-      <Text color={'white'}>
-        Exercise
-      </Text>
-    </Center>
+    <VStack flex={1}>
+      <VStack px={8} bg="gray.600" pt={12} >
+        <TouchableOpacity>
+          <Icon as={Feather} 
+            name='arrow-left' 
+            color="green.500" size={6} 
+            onPress={handleGoBack}/>  
+        </TouchableOpacity>
+      </VStack>
+    </VStack>
   );
 }
 
