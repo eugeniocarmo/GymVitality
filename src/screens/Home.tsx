12308dfa21f518c012288737ff75c0826e4bbcb4
Eugenio Carmo <eugeniocarmo@gmail.com>
2023-06-19 09:43:11 +0100

    feat: ' Navigating to exercise screen'

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { FlatList, HStack, Heading, Text, VStack } from 'native-base';
+
+import { AppNavigatorRoutesProps } from '@routes/app.routes';
 
 import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
@@ -12,6 +15,13 @@ export function Home() {
   const [groups, setGroups] = useState(['lower back', 'biceps', 'triceps', 'shoulders', 'upper body']);
 
   const [exercises, setExercises] = useState(['unilateral rowing', 'front pull', 'bent-over row', 'deadlift', '5','6']);
+
+
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+
+  function handleOpenExerciseDetails(){
+    navigation.navigate('exercise');
+  }
 
   return(
     <VStack flex={1}>
@@ -55,7 +65,9 @@ export function Home() {
             data={exercises}
             keyExtractor={item => item}
             renderItem={({item}) => (
-            <ExerciseCard />
+            <ExerciseCard 
+              onPress={handleOpenExerciseDetails}
+            />
             )}
             showsVerticalScrollIndicator={false}
             _contentContainerStyle={{ paddingBottom: 20 }}
