75d7fd48c160e5cac456ecebd4415eacc95b1ae4
Eugenio Carmo <eugeniocarmo@gmail.com>
2023-06-23 19:28:06 +0100

    fix: React Hook forms install

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,14 +1,15 @@
-import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
-
 import { useNavigation } from '@react-navigation/native';
-
+import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
 import LogoSvg from '@assets/logo1.svg';
 import BackgroundImg from '@assets/background.png';
 
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 
+
 export function SignUp(){
+
+
 
   const navigation = useNavigation();
 
