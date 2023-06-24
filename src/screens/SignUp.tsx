593bd97f8fddb4491e8f23c73971cf4fcba02d07
Eugenio Carmo <eugeniocarmo@gmail.com>
2023-06-24 15:38:17 +0100

    feat: Typing form data

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -8,14 +8,22 @@ import BackgroundImg from '@assets/background.png';
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 
+type FormDataProps = {
+   name: string;
+   email: string;
+   password: string;
+   password_confirm: string;
+
+};
+
 
 export function SignUp(){
-  const { control, handleSubmit } = useForm();
+  const { control, handleSubmit } = useForm<FormDataProps>();
 
   const navigation = useNavigation();
 
-  function handleSignUp(data: any){
-    console.log(data);
+  function handleSignUp( { name, password, email, password_confirm }: FormDataProps){
+    console.log({ name, password, email, password_confirm });
   }
 
 
