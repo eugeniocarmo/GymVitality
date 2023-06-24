6e746f7c401756d3f49dd5fd4d83621cc3a8d5a6
Eugenio Carmo <eugeniocarmo@gmail.com>
2023-06-24 21:32:48 +0100

    feat: Styling Invalid inputs

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -19,6 +19,11 @@ export function Input({errorMessage = null, isInvalid, ...rest}: Props ) {
         fontFamily="body"
         color="white"
         placeholderTextColor={'gray.300'}
+        isInvalid={invalid}
+        _invalid={{
+          borderWidth: 1,
+          borderColor: "red.500"
+        }}
 
         _focus={{
           bg:"gray.700",
