9c618c9bc5dd00e44ee8106d30397584a772dbcb
Eugenio Carmo <eugeniocarmo@gmail.com>
2023-07-04 21:03:28 +0100

    test: Testing code with Axios Interceptors

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,23 @@
 import axios from 'axios';
 
-export const api = axios.create({
+const api = axios.create({
   baseURL: 'http://169.254.172.166:3333'
 });
+
+// // Testing Interceptor request  
+// api.interceptors.request.use((config) => {
+//   console.log("DATA SENT =>", config.data);
+//   return config;
+// }, (error) => {
+//   return Promise.reject(error);
+// });
+
+//  Testing Interceptor response
+api.interceptors.response.use((response) => {
+  return response;
+}, (error) => {
+  console.log("INTERCEPTOR RESPONSE ERROR =>", error);
+  return Promise.reject(error);
+});
+
+export { api };
