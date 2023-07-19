d0c51a4451e1077b8d3be771cb7b71a6852fedd8
Eugenio Carmo <eugeniocarmo@gmail.com>
2023-07-19 23:03:20 +0100

    feat: Searching for user data in the backend

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -31,8 +31,8 @@ export function SignIn(){
     navigation.navigate('SignUp')
   }
 
-  function handleSignIn({ email, password }: FormData){
-    signIn(email, password);
+  async function handleSignIn({ email, password }: FormData){
+    await signIn(email, password);
   }
 
   return(
